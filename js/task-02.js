aafdcd60bb9967d58ccdde0b9d827682287972a1
host94 <host061994@gmail.com>
2023-07-02 22:22:59 +0300

    update v1.0

---
+++ js/task-02.js
@@ -0,0 +1,15 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ul = document.querySelector("#ingredients");
+ingredients.forEach((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  ul.append(li);
+});
